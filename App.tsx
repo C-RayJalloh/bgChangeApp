50dd421437f222f3dcf98f7e5fc8a52c7cc81570
“Jahrulo” <mserayjay@gmail.com>
2024-06-11 20:46:58 +0000

    "Formatting changes: whitespace and quote consistency adjustments"

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
   StatusBar,
   StyleSheet,
@@ -8,54 +8,50 @@ import {
 } from 'react-native';
 
 function App(): JSX.Element {
-  const [containerBackground, setContainerBackground] = useState("#ffffff");
-  
+  const [containerBackground, setContainerBackground] = useState('#ffffff');
+
   const generateColor = () => {
-    const hexRange = "0123456789ABCDEF";
-    let containerColor = "#";
-   
+    const hexRange = '0123456789ABCDEF';
+    let containerColor = '#';
 
     for (let i = 0; i < 6; i++) {
       containerColor += hexRange[Math.floor(Math.random() * 16)];
     }
 
-    
     setContainerBackground(containerColor);
-  
-  }
+  };
 
   return (
     <>
-    <StatusBar backgroundColor={containerBackground}/>
-    <View style={[styles.container, {backgroundColor: containerBackground}] }>
-      <TouchableOpacity onPress={generateColor}>
-        <View style={styles.actionBtn}>
-          <Text style={styles.actionBtnTxt}>Press me</Text>
-        </View>
-      </TouchableOpacity>
-      
-    </View>
+      <StatusBar backgroundColor={containerBackground} />
+      <View style={[styles.container, {backgroundColor: containerBackground}]}>
+        <TouchableOpacity onPress={generateColor}>
+          <View style={styles.actionBtn}>
+            <Text style={styles.actionBtnTxt}>Press me</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems: "center",
-    justifyContent: "center",
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   actionBtn: {
     borderRadius: 12,
-    backgroundColor: "#e31616",
+    backgroundColor: '#e31616',
     paddingVertical: 10,
-    paddingHorizontal: 40
+    paddingHorizontal: 40,
   },
   actionBtnTxt: {
     fontSize: 24,
-    color: "#FFFFFF",
-    textTransform: "uppercase"
-  }
+    color: '#FFFFFF',
+    textTransform: 'uppercase',
+  },
 });
 
 export default App;
